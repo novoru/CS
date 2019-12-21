a074b257bdf41688c9c785b2ce27992fa01b907b
Horita Noboru <61climb@gmail.com>
2019-12-21 21:39:43 +0900

    add A* search

--- 02/maze/generic_search.py
+++ 02/maze/generic_search.py
@@ -136,6 +136,44 @@ def bfs(intial: T, goal_test: Callable[[T], bool], successors: Callable[[T], Lis
             frontier.push(Node(child, current_node))
     return None
 
+class PriorityQueue(Generic[T]):
+    def __init__(self) -> None:
+        self._container: List[T] = []
+
+    @property
+    def empty(self) -> bool:
+        return not self._container
+
+    def push(self, item: T) -> None:
+        heappush(self._container, item)
+
+    def pop(self) -> T:
+        return heappop(self._container)
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+def astar(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], List[T]], heuristic: Callable[[T], float]) -> Optional[Node[T]]:
+    frontier: PriorityQueue[Node[T]] = PriorityQueue()
+    frontier.push(Node(initial, None, 0.0, heuristic(initial)))
+
+    explored: Dict[T, float] = {initial: 0.0}
+
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+
+        if goal_test(current_state):
+            return current_node
+        
+        for child in successors(current_state):
+            new_cost: float = current_node.cost + 1
+
+            if child not in explored or explored[child] > new_cost:
+                explored[child] = new_cost
+                frontier.push(Node(child, current_node, new_cost, heuristic(child)))
+    return None
+
 if __name__ == "__main__":
     print(linear_contains([1, 5, 15, 15, 15, 15, 20], 5))
     print(binary_contains(["a", "d", "e", "f", "z"], "f"))
--- 02/maze/maze.py
+++ 02/maze/maze.py
@@ -2,7 +2,7 @@ from enum import Enum
 from typing import List, NamedTuple, Callable, Optional
 import random
 from math import sqrt
-from generic_search import dfs, bfs, node_to_path, Node
+from generic_search import dfs, bfs, node_to_path, Node, astar
 
 class Cell(str, Enum):
     EMPTY = " "
@@ -68,6 +68,21 @@ class Maze:
         self._grid[self.start.row][self.start.column] = Cell.START
         self._grid[self.goal.row][self.goal.column] = Cell.GOAL
 
+def euclidean_distance(goal: MazeLocation) -> Callable[[MazeLocation], float]:
+    def distance(ml: MazeLocation) -> float:
+        xdist: int = ml.column - goal.column
+        ydist: int = ml.row - goal.row
+        return sqrt((xdist * xdist) + (ydist * ydist))
+    return distance
+
+def manhattan_distance(goal: MazeLocation) -> Callable[[MazeLocation], float]:
+    def distance(ml: MazeLocation) -> float:
+        xdist: int = abs(ml.column - goal.column)
+        ydist: int = abs(ml.row - goal.row)
+        return (xdist + ydist)
+    return distance
+
+
 if __name__ == "__main__":
     m: Maze = Maze()
     print(m)
@@ -89,3 +104,12 @@ if __name__ == "__main__":
         m.mark(path2)
         print(m)
         m.clear(path2)
+
+    distance: Callable[[MazeLocation], float] = manhattan_distance(m.goal)
+    solution3: Optional[Node[MazeLocation]] = astar(m.start, m.goal_test, m.successors, distance)
+    if solution3 is None:
+        print("No solution found using A*!")
+    else:
+        path3: List[MazeLocation] = node_to_path(solution3)
+        m.mark(path3)
+        print(m)
